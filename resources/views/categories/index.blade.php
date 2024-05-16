b80c80d11512c2a2c765b336a38a72dba564aa9f
Mariano Arias <marianoariasdev@gmail.com>
2024-05-16 11:50:29 -0300

    feat: Paginate category index page and add pagination links

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -61,6 +61,15 @@
                                 @endforeach
                             </tr>
                         </tbody>
+                        <tfoot class="mt-4">
+                            <tr>
+                                <td colspan="6">
+                                    <div class="px-6 py-3">
+                                        {{ $categories->links() }}
+                                    </div>
+                                </td>
+                            </tr>
+                        </tfoot>
                     </table>
                 </div>
             </div>
